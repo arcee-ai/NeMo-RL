5e2ea4d7d2c33dba073f2d195d69897bc2651b2c
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-08-14 15:45:04 -0700

    credit source for test env

--- nemo_rl/environments/verifiers/alphabet_env.py
+++ nemo_rl/environments/verifiers/alphabet_env.py
@@ -1,3 +1,9 @@
+"""
+This file was originally created for Prime Intellect's prime-rl.
+
+https://github.com/PrimeIntellect-ai/prime-rl/blob/main/environments/vf_alphabet_sort/vf_alphabet_sort.py
+"""
+
 import difflib
 import json
 import random
