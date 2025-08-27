1b161f4aeddd0213b132d0aac04b65f7b8cea04e
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-08-26 18:51:36 -0700

    use config type instead

--- nemo_rl/models/custom/convert.py
+++ nemo_rl/models/custom/convert.py
@@ -1,6 +1,6 @@
 from typing import Callable
 from torch import nn
-from transformers import PreTrainedModel
+from transformers import PreTrainedModel, PretrainedConfig
 
 from nemo_rl.models.custom.model import BaseModelArgs
 
@@ -16,36 +16,36 @@ from nemo_rl.models.custom.qwen3.parallelize import parallelize_qwen3
 
 from nemo_rl.models.custom.state_dict_adapter import BaseStateDictAdapter
 
-def get_model_config(model: PreTrainedModel) -> tuple[type[nn.Module], BaseModelArgs, type[BaseStateDictAdapter], Callable]:
-    mt = model.config.model_type
+def get_model_config(config: PretrainedConfig) -> tuple[type[nn.Module], BaseModelArgs, type[BaseStateDictAdapter], Callable]:
+    mt = config.model_type
     
     if mt == "llama":
         # TODO: verfy this works on all llama3 models
         return LlamaModel, LlamaModelArgs(
-            dim = model.config.hidden_size,
-            n_layers = model.config.num_hidden_layers,
-            n_heads = model.config.num_attention_heads,
-            n_kv_heads = model.config.num_key_value_heads,
-            vocab_size = model.config.vocab_size,
-            norm_eps = model.config.layer_norm_eps,
-            rope_theta = model.config.rope_theta,
-            max_seq_len = model.config.max_position_embeddings,
-            eos_id = model.config.eos_token_id
+            dim = config.hidden_size,
+            n_layers = config.num_hidden_layers,
+            n_heads = config.num_attention_heads,
+            n_kv_heads = config.num_key_value_heads,
+            vocab_size = config.vocab_size,
+            norm_eps = config.layer_norm_eps,
+            rope_theta = config.rope_theta,
+            max_seq_len = config.max_position_embeddings,
+            eos_id = config.eos_token_id
         ), Llama3StateDictAdapter, parallelize_llama
     elif mt == "qwen3":
         return Qwen3Model, Qwen3ModelArgs(
-            dim = model.config.hidden_size,
-            n_layers = model.config.num_hidden_layers,
-            n_heads = model.config.num_attention_heads,
-            n_kv_heads = model.config.num_key_value_heads,
-            vocab_size = model.config.vocab_size,
-            head_dim = model.config.head_dim,
-            hidden_dim = model.config.intermediate_size,
-            norm_eps = model.config.rms_norm_eps,
-            rope_theta = model.config.rope_theta,
-            max_seq_len = model.config.max_position_embeddings,
-            eos_id = model.config.eos_token_id,
-            enable_weight_tying = model.config.tie_word_embeddings
+            dim = config.hidden_size,
+            n_layers = config.num_hidden_layers,
+            n_heads = config.num_attention_heads,
+            n_kv_heads = config.num_key_value_heads,
+            vocab_size = config.vocab_size,
+            head_dim = config.head_dim,
+            hidden_dim = config.intermediate_size,
+            norm_eps = config.rms_norm_eps,
+            rope_theta = config.rope_theta,
+            max_seq_len = config.max_position_embeddings,
+            eos_id = config.eos_token_id,
+            enable_weight_tying = config.tie_word_embeddings
         ), Qwen3StateDictAdapter, parallelize_qwen3
     else:
         raise ValueError(f"Model type {mt} unknown or not supported")
