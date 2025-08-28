d46d4ed4a20765502dfe92045805ac6b48044f1e
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-08-28 14:00:04 -0700

    use terrible hf moe impl too, sanity check

--- nemo_rl/models/custom/qwen3moe/model.py
+++ nemo_rl/models/custom/qwen3moe/model.py
@@ -1,3 +1,4 @@
+from sympy.logic import true
 import torch
 import torch.nn as nn
 
@@ -41,6 +42,59 @@ class Qwen3MoeMLP(nn.Module):
         down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
         return down_proj
 
+class Qwen3MoeSparseMoeBlock(nn.Module):
+    def __init__(self, num_experts: int, num_experts_per_tok: int, norm_topk_prob: bool, dim: int, hidden_dim: int, moe_hidden_dim: int):
+        super().__init__()
+        self.num_experts = num_experts
+        self.top_k = num_experts_per_tok
+        self.norm_topk_prob = norm_topk_prob
+
+        # gating
+        self.gate = nn.Linear(dim, num_experts, bias=False)
+        self.experts = nn.ModuleList(
+            [Qwen3MoeMLP(dim, moe_hidden_dim) for _ in range(self.num_experts)]
+        )
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        """ """
+        batch_size, sequence_length, hidden_dim = hidden_states.shape
+        hidden_states = hidden_states.view(-1, hidden_dim)
+        # router_logits: (batch * sequence_length, n_experts)
+        router_logits = self.gate(hidden_states)
+
+        routing_weights = torch.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+        if self.norm_topk_prob:  # only diff with mixtral sparse moe block!
+            routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+        # we cast back to the input dtype
+        routing_weights = routing_weights.to(hidden_states.dtype)
+
+        final_hidden_states = torch.zeros(
+            (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
+        )
+
+        # One hot encode the selected experts to create an expert mask
+        # this will be used to easily index which expert is going to be sollicitated
+        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+
+        # Loop over all available experts in the model and perform the computation on each expert
+        expert_hitted = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
+        for expert_idx in expert_hitted:
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx].squeeze(0))
+
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+        final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+        return final_hidden_states
+
 class TransformerBlock(nn.Module):
     def __init__(self, layer_id: int, model_args: Qwen3MoEModelArgs):
         super().__init__()
@@ -61,7 +115,14 @@ class TransformerBlock(nn.Module):
         
         if self.moe_enabled:
             # For MoE layers, experts operate on model hidden size (dim) and expand to moe_intermediate_size
-            self.moe = MoE(model_args.moe_args, model_args.dim, model_args.moe_intermediate_size)
+            #self.moe = MoE(model_args.moe_args, model_args.dim, model_args.moe_intermediate_size)
+            self.moe = Qwen3MoeSparseMoeBlock(
+                model_args.moe_args.num_experts,
+                model_args.moe_args.top_k,
+                True,
+                model_args.dim, model_args.hidden_dim,
+                model_args.moe_intermediate_size
+            )
         else:
             self.feed_forward = Qwen3MoeMLP(model_args.dim, model_args.hidden_dim)
         
