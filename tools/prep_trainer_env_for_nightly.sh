704be9f184105307a6fdc47d048cb0ca02f71f75
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-08-29 12:46:43 -0700

    correct venv path

--- tools/prep_trainer_env_for_nightly.sh
+++ tools/prep_trainer_env_for_nightly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install flash-attn and vllm
-VENV_PATH="nemo_rl.models.policy.dtensor_v2.v2_policy_worker.DTensorV2PolicyWorker"
+VENV_PATH="venvs/nemo_rl.models.policy.dtensor_v2.v2_policy_worker.DTensorV2PolicyWorker"
 uv pip install -U --prerelease allow torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128 -p $VENV_PATH
 uv pip install git+https://github.com/Dao-AILab/flash-attention.git@v2.8.3 -p $VENV_PATH --no-build-isolation
 uv pip install git+https://github.com/vllm-project/vllm.git@v0.10.1.1 -p $VENV_PATH --no-build-isolation
