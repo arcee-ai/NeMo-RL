6d8cf3e80deec6f6d826e5f0e9ca13f12ad2bcb9
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-08-29 12:46:26 -0700

    update env prep

--- tools/prep_base_env_for_nightly.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install flash-attn and vllm
-uv pip install flash-attn vllm
-uv pip install -U --prerelease allow torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
