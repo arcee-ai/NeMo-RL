ccca263336be0a2babe470874568b877b0ad75ec
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-03 19:10:56 -0700

    disable fullgraph

--- nemo_rl/models/custom/qwen3moe/parallelize.py
+++ nemo_rl/models/custom/qwen3moe/parallelize.py
@@ -95,7 +95,7 @@ def parallelize_qwen3moe(
     # compile each layer
     torch._dynamo.config.capture_scalar_outputs = True
     for layer_name, layer in model.layers.items():
-        layer = torch.compile(layer, fullgraph=True)
+        layer = torch.compile(layer, fullgraph=False)
         model.layers.register_module(layer_name, layer)
 
     fsdp_config = {
