848ea85914f094848343fe82c3464a28a214a72f
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-12 15:45:52 -0700

    keep flat ep

--- nemo_rl/models/policy/dtensor_v2/v2_policy_worker.py
+++ nemo_rl/models/policy/dtensor_v2/v2_policy_worker.py
@@ -406,6 +406,7 @@ class DTensorV2PolicyWorker:
         self.dp_mesh = device_mesh[list(dp_names)]._flatten(mesh_dim_name="dp")
         self.dp_shard_cp_mesh = device_mesh[list(dp_shard_cp_names)]._flatten(mesh_dim_name="dp_shard_cp")
         self.dp_cp_mesh = device_mesh[list(dp_cp_names)]._flatten(mesh_dim_name="dp_cp")
+        device_mesh[list(ep_names)]._flatten(mesh_dim_name="ep")
         self.ep_mesh = device_mesh[list(ep_names)]
 
         self.pp_mesh, self.tp_mesh, self.cp_mesh = (
