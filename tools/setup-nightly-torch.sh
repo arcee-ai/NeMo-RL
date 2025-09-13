4bc2335a53b6fea7394462cfe53982fcf1a979fd
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-12 19:20:38 -0700

    no deps on flash-attn either

--- tools/setup-nightly-torch.sh
+++ tools/setup-nightly-torch.sh
@@ -8,7 +8,7 @@ uv pip install -P torch --prerelease allow torch torchvision torchaudio --index-
 
 bash tools/build-vllm-with-nightly.sh
 
-uv pip install -n -P flash-attn flash-attn
+uv pip install -n -P flash-attn flash-attn --no-deps
 
 if [ -d "$VENV_DIR" ]; then
     echo "Using existing venv..."
@@ -21,6 +21,6 @@ uv sync -p $VENV_DIR --extra vllm_http
 uv pip install -P torch --prerelease allow torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128 -p $VENV_DIR
 uv pip install -r 3rdparty/vllm/requirements/build.txt -p $VENV_DIR
 uv pip install -e 3rdparty/vllm -p $VENV_DIR --no-deps
-uv pip install -P flash-attn flash-attn -p $VENV_DIR
+uv pip install -P flash-attn flash-attn -p $VENV_DIR --no-deps
 
 echo "Nightly torch setup complete! From now on, run scripts with 'uv run --no-sync'"
