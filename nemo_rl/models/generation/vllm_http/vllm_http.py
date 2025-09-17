95c74366de420348c304af91bf44889ac7e3a8c0
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-16 22:17:14 -0700

    remove ineffectual change

--- nemo_rl/models/generation/vllm_http/vllm_http.py
+++ nemo_rl/models/generation/vllm_http/vllm_http.py
@@ -35,7 +35,6 @@ class VLLMOpenAIServe:
             "--logprobs-mode", "processed_logprobs",
             "--gpu-memory-utilization", str(gpu_memory_utilization),
             "--data-parallel-size", str(data_parallel_size),
-            "--disable-log-requests",
         ]
         if extra_cli_args:
             args += extra_cli_args
