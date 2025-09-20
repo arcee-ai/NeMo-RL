dc6fa98753774ddb540bec2fafbf60bb5563898b
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-20 00:07:57 -0700

    give vLLM more CPUs

--- nemo_rl/models/generation/vllm_http/vllm_http_generation.py
+++ nemo_rl/models/generation/vllm_http/vllm_http_generation.py
@@ -75,7 +75,7 @@ class VllmHttpGeneration(GenerationInterface):
 
         vllm_app = VLLMOpenAIServe.options( # type: ignore
             ray_actor_options={
-                "num_cpus": 1,
+                "num_cpus": 4,
                 "runtime_env": runtime_env,
             },
         ).bind(
