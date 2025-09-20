502bdaebac98e7912206f6753d20ceb78d7315ce
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-20 00:03:43 -0700

    change max batch size

--- nemo_rl/models/generation/vllm_http/vllm_http.py
+++ nemo_rl/models/generation/vllm_http/vllm_http.py
@@ -50,8 +50,7 @@ class VLLMOpenAIServe:
             "--max-model-len", str(max_model_len),
             "--logprobs-mode", "processed_logprobs",
             "--gpu-memory-utilization", str(gpu_memory_utilization),
-            "--data-parallel-size", str(data_parallel_size),
-            "--tokenizer-pool-size", "8"
+            "--data-parallel-size", str(data_parallel_size)
         ]
         if tool_call_parser:
             args += ["--tool-call-parser", tool_call_parser]
