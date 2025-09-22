0bede27196f948a46b92c290d3d426eff445127c
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-22 12:31:41 -0700

    remove reference to grouped rubric

--- env_api/vf_exts/vf_exts/__init__.py
+++ env_api/vf_exts/vf_exts/__init__.py
@@ -1,7 +1,6 @@
 __version__ = "0.1.0"
 
 from .env.mt_env_group import MultiTurnEnvGroup
-from .rubric.grouped_rubric import GroupedRubric
 from .rubric.btrm_judge import PairwiseJudgeRubric
 
-__all__ = ["MultiTurnEnvGroup", "GroupedRubric", "PairwiseJudgeRubric"]
+__all__ = ["MultiTurnEnvGroup", "PairwiseJudgeRubric"]
