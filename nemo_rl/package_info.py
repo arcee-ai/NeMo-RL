600317896bdbdc2be9641ea0b9121524a5cee3d3
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-09-29 21:14:16 +0000

    bump version

--- nemo_rl/package_info.py
+++ nemo_rl/package_info.py
@@ -14,9 +14,9 @@
 
 
 MAJOR = 1
-MINOR = 0
-PATCH = 1
-PRE_RELEASE = ""
+MINOR = 1
+PATCH = 0
+PRE_RELEASE = "dev"
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
