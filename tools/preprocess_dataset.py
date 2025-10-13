44d28c380f8bbbe00d4b923440b432fb82ac4d31
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-10-13 20:03:33 +0000

    update preprocessor to upload to hub

--- tools/preprocess_dataset.py
+++ tools/preprocess_dataset.py
@@ -2,7 +2,7 @@ import argparse
 import logging
 from datasets import load_dataset
 from transformers import AutoTokenizer
-from rlkit.algorithms.sft import transform_dataset
+from rlkit.data.datasets import transform_dataset
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     parser.add_argument("output_path", type=str)
     parser.add_argument("--tokenizer-name", type=str, required=False, default=None)
     parser.add_argument("--num-proc", type=int, default=8)
+    parser.add_argument("--push-to-hub", action="store_true")
+    parser.add_argument("--hf-private", action="store_true")
     args = parser.parse_args()
     
     # Load tokenizer
@@ -23,10 +25,15 @@ if __name__ == "__main__":
     
     dataset = load_dataset(args.dataset_name)
     logging.info(f"Loaded dataset with splits {list(dataset.keys())}. Applying transformation...")
+    
     for split in dataset.keys():
         logging.info(f"Transforming split '{split}'")
         dataset[split] = transform_dataset(dataset[split], args.dataset_type, tokenizer, num_proc=args.num_proc)
     
-    logging.info(f"Saving dataset to '{args.output_path}'")
-    dataset.save_to_disk(args.output_path)
+    if not args.push_to_hub:
+        logging.info(f"Saving dataset to '{args.output_path}'")
+        dataset.save_to_disk(args.output_path)
+    else:
+        logging.info(f"Pushing dataset to Hugging Face repo '{args.output_path}'...")
+        dataset.push_to_hub(args.output_path, private=args.hf_private, num_proc=args.num_proc)
     
